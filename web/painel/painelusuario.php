9e0b90d1abfb932a60067677c7076fe44e1fc48c
41404998 <41404998@090017-P33S13.macklabs.br>
2015-03-12 11:27:07 -0300

    Inclusão do painel de usuario

---
+++ web/painel/painelusuario.php
@@ -0,0 +1,13 @@
+<?php include '../header.php'; ?>
+<body>
+    <h1> Paínel do Usuário </h1>
+    <ul class="painelusuario">
+        <li> Meus Uploads </li>
+        <li> Meu Cadastro </li>
+        <li> Minha lista de desejos </li>
+    </ul>
+</body>
+<footer>
+    <?php include '../footer.php';?>
+</footer>
+
