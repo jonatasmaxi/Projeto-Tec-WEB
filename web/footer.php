8464df255f93a50361e5f31be981cbda4b6977ab
Lucas Tedeschi <lucastedeschi@outlook.com>
2015-03-12 11:02:14 -0300

    Criação da pasta web Criação do header.php, footer.php, index.php e style.css

---
+++ web/footer.php
@@ -0,0 +1,11 @@
+ <footer>
+            <div class="copyright">
+            </div>    
+            <nav>
+                <ul>
+                    <li></li>
+                </ul>
+            </nav>
+        </footer>
+    </body>
+</html>
---
+++ web/index.php
@@ -0,0 +1,42 @@
+<?php include 'header.php'?>
+        <section>
+            <button onclick="#"></button>
+            <aside>
+                <nav class="departments">
+                    <ul>
+                        <li></li>
+                    </ul>
+                </nav>
+            </aside>
+            <div class="slide">
+                
+            </div>
+            <div class="gridLast">
+                <table class="tableLast">
+                    <tr class="tableLastHeader">
+                        <th colspan="2">Últimos produtos adicionados<th>
+                        <th></th>    
+                    </tr>
+                    <tr class="tableLastProduct">
+                        <td></td>
+                        <td></td> 
+                        <td></td>
+                    </tr>
+                </table>
+            </div>
+            <div class="topUsers">
+                <table class="tableUsers">
+                    <tr class="tableUsersHeader">
+                        <th colspan="2">Top usuários<th>
+                        <th></th>    
+                    </tr>
+                    <tr class="tableUsersTop">
+                        <td></td>
+                        <td></td> 
+                        <td></td>
+                    </tr>
+                </table>
+            </div>
+        </section>
+<?php include 'footer.php'?>
+       
