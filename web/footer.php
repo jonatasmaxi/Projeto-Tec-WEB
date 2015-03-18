ec420564a941adfb2185027d9b8a36734724b37a
41406133 <41406133@089896-P33S13.macklabs.br>
2015-03-18 10:37:42 -0300

    Tabela de cores Mudanças no index.php

--- web/footer.php
+++ web/footer.php
@@ -1,4 +1,4 @@
- <footer>
+<footer>
             <div class="copyright">
             </div>    
             <nav>
--- web/header.php
+++ web/header.php
@@ -2,7 +2,7 @@
 <html>
     <head>
         <meta charset="UTF-8">
-        <title> Cheaper </title>
+        <title>Cheaper </title>
         <link rel="stylesheet" href="style.css"/>
     </head>
     <body>
