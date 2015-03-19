4ba4623ba83e05e6be821c787a443bd12a72c347
41404998 <41404998@090017-P33S13.macklabs.br>
2015-03-19 11:49:33 -0300

    Tentativa 2 de validação de formulário

--- web/painel/recebe_dados.php
+++ web/painel/recebe_dados.php
@@ -1,10 +1,25 @@
+<!DOCTYPE HTML>
+<meta charset="UTF-8">
 <?php 
+     $erro=0;
      $nome = $_POST["nome"];
      $sobrenome = $_POST["sobrenome"];
      $cidade = $_POST["cidade"];
      $senha = $_POST["senha"];
      $confirmasenha = $_POST["confirmasenha"] ;
      $nascimento = $_POST['nascimento'];
-     if (empty($nome))
-       {echo "Favor digitar seu nome corretamente.<br>"; $erro=1;}
+     $email = $_POST['email'];
+     if (empty($nome) )
+        {echo "O campo nome não pode estar vazio.<br>"; $erro++;}
+     if (empty($sobrenome))
+       {echo "O campo sobrenome não pode estar vazio.<br>"; $erro++;}
+     if (empty($cidade))
+       {echo "O campo cidade não pode estar vazio.<br>"; $erro++;}
+     if (empty($senha))
+       {echo "O campo senha não pode estar vazio.<br>"; $erro++;}
+     if (empty($confirmasenhasenha))
+       {echo "O campo confirma a senha não pode estar vazio.<br>"; $erro++;}
+     if (empty($email))
+        {echo "O campo email não pode estar vazio.<br>"; $erro++;}
+       
 ?>
