5788c8585054436d317a8ef368cae3fb8dcb58f4
41404998 <41404998@090017-P33S13.macklabs.br>
2015-03-19 11:49:49 -0300

    Tentativa 3 de validação de formulário

--- web/painel/Formulariodecadastro.php
+++ web/painel/Formulariodecadastro.php
@@ -3,12 +3,12 @@
     <body>
         <section>
             <div>
-                <form name="cadastro" id="form" method="post" action="recebe_dados.php">
+                <form name="cadastro" id="form" method="post" action="Formulariodecadastro.php">
                     <label for="nome"> Nome*: </label> <input type="text" id="nome" name="nome"><br><br>
-                    <label for="sobrenome"> Sobrenome*: </label> <input type="text" id="sobrenome"> <br><br>
+                    <label for="sobrenome"> Sobrenome*: </label> <input type="text" name="sobrenome" id="sobrenome"> <br><br>
                     <label for="foto"> Foto: </label> <input type="file" id="foto"> <br> <br>
                     Sexo*: <input type="radio" value="masculino"> Masculino  <input type="radio" value="feminino"> Feminino <br><br>
-                    <label for="nascimento"> Data de Nascimento:* </label><input type="date" id="nascimento"><BR> <BR>
+                    <label for="nascimento"> Data de Nascimento:* </label><input type="date" name="nascimento" id="nascimento"><BR> <BR>
                     Estado: <select name="estado">
                         <option value="AC"> AC </option>
                         <option value="AL"> AL </option>
@@ -38,12 +38,35 @@
                         <option value="SE"> SE </option>
                         <option value="TO"> TO </option>
                     </select>
-                    <label for="cidade"> Cidade*: </label> <input type="text" id="cidade"> <br><br>
-                    <label for="email">E-mail*: </label> <input type="text" id="email"><br><br>
-                    <label for="senha"> Senha*: </label> <input type="password" id="senha"><br><br>
-                    <label for="confirmasenha"> Confirme a Senha*: </label> <input type="password" id="confirmasenha">
-                    <br> <br> <input type="button" value="Enviar"> 
+                    <label for="cidade"> Cidade*: </label> <input type="text" name="cidade" id="cidade"> <br><br>
+                    <label for="email">E-mail*: </label> <input type="text" name="email" id="email"><br><br>
+                    <label for="senha"> Senha*: </label> <input type="password" name="senha" id="senha"><br><br>
+                    <label for="confirmasenha"> Confirme a Senha*: </label> <input type="password" name="confirmasenha"id="confirmasenha">
+                    <br> <br> <input type="submit" value="Enviar"> 
                     <input type="button" value="Cancelar">
+                    <?php 
+                        $erro=0;
+                        $nome = $_POST["nome"];
+                        $sobrenome = $_POST["sobrenome"];
+                        $cidade = $_POST["cidade"];
+                        $senha = $_POST["senha"];
+                        $confirmasenha = $_POST["confirmasenha"] ;
+                        $nascimento = $_POST['nascimento'];
+                        $email = $_POST['email'];
+                        if (empty($nome) )
+                           {echo "O campo nome não pode estar vazio.<br>"; $erro++;}
+                        if (empty($sobrenome))
+                          {echo "O campo sobrenome não pode estar vazio.<br>"; $erro++;}
+                        if (empty($cidade))
+                          {echo "O campo cidade não pode estar vazio.<br>"; $erro++;}
+                        if (empty($senha))
+                          {echo "O campo senha não pode estar vazio.<br>"; $erro++;}
+                        if (empty($confirmasenhasenha))
+                          {echo "O campo confirma a senha não pode estar vazio.<br>"; $erro++;}
+                        if (empty($email))
+                           {echo "O campo email não pode estar vazio.<br>"; $erro++;}
+
+                    ?>
                 </form>
             </div>
       </section>
