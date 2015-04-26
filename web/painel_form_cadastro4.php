be3166b4cf5309353e81106e43c1d675154ca695
Jon <Jon@Jon-PC>
2015-04-26 15:40:11 -0300

    Finalização da validação do cadastro

---
+++ web/painel_form_cadastro4.php
@@ -0,0 +1,10 @@
+<!DOCTYPE HTML>
+<html>
+<?php include 'header.php' ?>
+<?php include 'aside.php' ?>
+    <h1>
+        Cadastro Realizado com Sucesso <br> <br>
+    </h1>
+    <a href="index.php"> Retornar à página principal </a>
+<?php include 'footer.php'?>
+</html>
